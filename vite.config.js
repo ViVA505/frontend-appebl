47c087e280c9ff58cbff5247a9b922929253fc8a
ViVA505 <vladimirrezume5@gmail.com>
2025-11-04 13:48:51 +0300

    ref config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -14,5 +12,12 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
+  },
+  // 👇 добавляем это
+  server: {
+    historyApiFallback: true,
+  },
+  build: {
+    outDir: 'dist', // убедись, что деплоишь именно из этой папки
   },
 })
